7f2c07f5a310e24cc671bc3a623036af20c3da5f
Hanlu Ye class of 2015 <hye@tempest.wellesley.edu>
2014-04-19 14:39:09 -0400

    Create Team basic cgi script and accompanying files. Currently connected to lu's database.

---
+++ createTeam.py
@@ -0,0 +1,48 @@
+#!/usr/local/bin/python2.7
+
+
+''' This module consists of Presence helper functions to insert a team into a database.
+ May be merged with other modules later. 
+
+Created by Lulu Ye - 19 April 2014'''
+
+
+import MySQLdb
+from lu_db import DSN #change
+import dbconn
+import cgi
+import cgi_utils_sda
+
+global conn #declaring global conn
+global curs #declaring global
+
+''' Called on submit '''
+def submit(form_data):
+    print "submit method in createTeam.py"
+    #connect to the database
+    global conn, curs
+    conn = connect()
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    # Retrieve and escape the necessary data to insert into the database
+    manager = form_data.getfirst("teamManager")
+    name = cgi.escape(form_data.getfirst("teamName")) #escape
+    location = cgi.escape(form_data.getfirst("location")) #escape
+    
+
+    createTeam(manager,name,location)
+    
+    
+''' Creates a team by executing a SQL insert statement.'''
+def createTeam(manager,name,loc):
+    global curs
+    curs.execute('INSERT INTO team(manager,name, location) VALUES(%s,%s,%s)',(manager, name, loc))
+    print ("<p>Your team has been created")
+
+
+''' Creates a database connection. '''
+def connect():
+    DSN['database']= 'hye_db' #change
+    conn = dbconn.connect(DSN)
+    conn.autocommit(True)
+    return conn
---
+++ lu_db.py
@@ -0,0 +1,8 @@
+
+
+DSN = {'hostname': 'localhost',
+       'username': 'hye',
+       'password': 'mTFKh6AbU4JFUQM',
+       'database': 'hye_db',
+       'phptype': 'mysql'}
+
