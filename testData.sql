65a1175828f0bd3aa2df9e4426717ceabc2625b9
Hanlu Ye class of 2015 <hye@tempest.wellesley.edu>
2014-05-03 20:22:29 -0400

    Modified testData for more robust testing

--- testData.sql
+++ testData.sql
@@ -10,15 +10,20 @@ DELETE from player;
 
 
 -- create some users
-INSERT INTO user(name,phnum) VALUES("Tori", 12345678910);
-INSERT INTO user(name,phnum) VALUES("Lulu", 10987654321);
+INSERT INTO user(email, name, dob, nickname) VALUES ("harry@hogwarts.com", "Harry Potter", 31-07-1980, "The Chosen One");
+INSERT INTO user(email, name, dob, nickname) VALUES ("ronald@hogwarts.com", "Ron Weasley", 01-03-1980, "Won Won");
+INSERT INTO user(email, name, dob, nickname) VALUES ("hermione@hogwarts.com", "Hermione Granger", 15-09-1979, "Hermy");
 
 -- create a team
-INSERT INTO team(name, location) VALUES("Rugsbee", "Wellesley");
+-- INSERT INTO team(name, location) VALUES("Rugsbee", "Wellesley");
+INSERT INTO team(name, manager, location) VALUES("DA", 3, "Hogwarts"); -- hopefully hermione is managing
 
 
--- add players onto team
--- INSERT INTO player() VALUES();
+-- add players onto team 
+INSERT INTO player(PID, team, position) VALUES(1, 1, "Seeker"); -- Harry
+INSERT INTO player(PID, team, position) VALUES(2, 1, "Keeper"); -- Ron
+INSERT INTO player(PID, team, position) VALUES(3, 1, "Chaser"); -- Hermione
+
 
 -- create event
--- INSERT INTO event() VALUES();
+INSERT INTO event(host_id, location) VALUES(1, "Astronomy Tower"); -- hopefully hosted by Hogwarts
