aabea0aa7c8baa362cb79d39ac1d3603e20be1f8
Hanlu Ye class of 2015 <hye@tempest.wellesley.edu>
2014-05-12 08:16:04 -0400

    Used some hidden fields so that a user can update her attendance

--- viewEvents.py
+++ viewEvents.py
@@ -26,10 +26,12 @@ def submit(form_data,submit_type):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
 
     if (submit_type == "Update Attendance"):
-        return updateAttendance("yes") #for now just maybe
+            
+        #retrieves the data from the form to update the database
+        return updateAttendance(form_data) #for now just maybe
         #execute Attendance thing
     else:
-    #retrieves the data from the form for the sql query
+    #retrieves the data from the form for the sql query for events
         view = form_data.getfirst("view") #whether in team or user mode
         id = form_data.getfirst("ID")
     
@@ -41,7 +43,6 @@ def submit(form_data,submit_type):
 # Fetches the events of a given team
 def getEvent(id,view):
     global curs
-    
     #print "<p> right outside of checking for view: " #debugging
     #print view #debugging
     #user event query
@@ -50,10 +51,17 @@ def getEvent(id,view):
     elif (view == "user"):
         #print "<p>hello! Querying for user events for id No. " + id #debugging
         curs.execute('SELECT * FROM event,(SELECT * FROM attend WHERE UID = %s) as userEv where userEv.EID = event.EID', (id,))
+        #printUserTable
 
     else:
-    #team event query - set as default
+    #team event query - currently set as default
         curs.execute('SELECT * FROM event WHERE host_id = %s', (id,))
+        #printTeamTable
+
+    ''' We need to differentiate between the user and the team tables.
+    The user table will include an option to attend an event, and the team
+    will have the option of viewing the eventss.'''
+
 
         # HTML Formatting below 
     header = "<div class=\"container\"><h2> Events for " + str(view) + " no. " + str(id) +  "</h2> \n <hr>"
@@ -67,8 +75,9 @@ def getEvent(id,view):
         #print "<p>curs.fetchone: " #debugging
         #print row #debugging
 
-        '''Advanced functionality of this would include using JSON to provide a sortable view of the events. We can implement this
-        In the future.'''
+        '''Advanced functionality of this would include using JSON to 
+        provide a sortable view of the events. We can implement this
+        in the future.'''
         
         if row == None:
             # print "<h2> Events </h2>" + "\n".join(lines) #debugging 
@@ -78,7 +87,7 @@ def getEvent(id,view):
         lines.append("<tr>" + "<td>" +  str(row.get('host_id')) + "</td>")
         lines.append("<td>" + str(row.get('location')) + "</td>")
         lines.append("<td>" + str(row.get('event_date')) + "</td>")
-        lines.append(printAttendRadio())
+        lines.append(printAttendRadio(id,str(row.get('EID')))) #user--assuming that this is only for the user, #event
         # lines.append("<td><form> <input type=\"radio\" name=\"attend\" value=\"yes\"> yes </form> </td> </tr>")
         # print lines #debugging
 
@@ -88,27 +97,59 @@ def printHtmlRow(host, loc, date):
     # fill this in later
     return html
 
-#prints the table data cell of the radio button
-def printAttendRadio(): 
+#prints the table data cell of the radio button.
+# requires the EID and the UID for the user
+def printAttendRadio(user,event): 
     html = "<td><form method=\"post\" action=\"viewEvents.cgi\">" # runs the viewEvents.cgi
-    html +="<input type=\"radio\" name=\"attend\" value=\"yes\" checked> yes" #yes option
-    html += "<input type=\"radio\" name=\"attend\" value=\"no\"> no" #no option
-    html +="<input type=\"radio\" name=\"attend\" value=\"maybe\"> maybe" #maybe option
-    html +="<input type=\"submit\" name=\"submit\" value=\"Update Attendance\"> " #submit button
+    html +="<input type=\"radio\" name=\"attend\" value=\"yes\" checked> yes\n" #yes option
+    html += "<input type=\"radio\" name=\"attend\" value=\"no\"> no\n" #no option
+    html +="<input type=\"radio\" name=\"attend\" value=\"maybe\"> maybe\n" #maybe option
+    html +="<input type=\"submit\" name=\"submit\" value=\"Update Attendance\"> \n" #submit button
+    html +="<input type=\"hidden\" name=\"event\" value=\""+ event + "\">\n" #encodes the event type
+    html +="<input type=\"hidden\" name=\"user\" value=\"" + user + "\">\n" #encodes the userID. Might not need this later with sessions
+
+    #hidden value for the event id, always returns 1 at this point
     # Also add a comment box later
     html +="</form></td>"
     return html
 
-def updateAttendance(response):
+
+
+# HTML method. Print the form and the table header for the user
+def printUserTable():
+    #execute the SQL query
+
+# HTML method Prints the form and the table header for the team table.
+def printTeamTable():
+    #execute the SQL query
+
+
+
+# Updates the attendance for only one event
+def updateAttendance(form_data):
+    ''' debugging
+    print("Your attendance form contained")    
+    for k in form_data:
+        print("{key} => {value}".format(key=k,value=form_data.getfirst(k)))
+    '''
+    response = form_data.getfirst("attend")
+    UID = form_data.getfirst("uid") #gets the userID, later might not be needed because of sessions
+    EID = form_data.getfirst("event") #gets the eventID for updating database
+    
+
     global curs
     msg =""
     if (response == "yes"):
-        msg = "<p>yes, I'm coming!"
+        msg = "<p>yes, I'm coming to event number " + EID
+
+#insert into the attend table that the user is attending the event
+        curs.execute('INSERT INTO attend VALUES(%s, %s)', (EID,UID))
         
         #execute a sql query
     if (response == "no"):
-        msg = "<p>no, I'm busy"
-    
+        msg = "<p>no, I'm busy. Deleting my presence from event number " + EID
+        curs.execute('DELETE FROM attend WHERE EID = %s and UID = %s', (EID,UID))
+        
     return msg + "<p>end of updateAttendance"
             #do nothing
     # if yes, then add the event and the user to the attends table
